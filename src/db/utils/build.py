22b336b489924a03f6d0d09e7c832896b22b57fb
Iwo Polański <outlying@gmail.com>
2025-11-15 13:24:09 +0100

    Working imports

--- src/db/utils/build.py
+++ src/db/utils/build.py
@@ -1,59 +1,53 @@
 import csv
-import sqlite3
-import sys
+import logging
 
-from src.db import SQLITE_DATABASE, SQLITE_SCHEMA, IMPORT_DATA_DIRECTORY
+from peewee import *
 
+from src.db import SQLITE_DATABASE, IMPORT_DATA_DIRECTORY
+from src.db.consts import TABLE_ITEMS
+from src.db.utils.mappers import MAPPERS
 
-def recreate_db():
+db = SqliteDatabase(SQLITE_DATABASE)
 
-    if SQLITE_DATABASE.exists():
-        SQLITE_DATABASE.unlink()
+logger = logging.getLogger(__name__)
 
-    conn = sqlite3.connect(SQLITE_DATABASE)
+class BaseModel(Model):
+    class Meta:
+        database = db
 
-    with open(SQLITE_SCHEMA, "r", encoding="utf-8") as f:
-        schema_sql = f.read()
-    conn.executescript(schema_sql)
-    conn.commit()
+class Item(BaseModel):
+    slug = CharField()
+    name = CharField()
 
-    return conn
-
-def read_csv_rows(csv_path):
-    with open(csv_path, newline="", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        rows = list(reader)
-    if not rows:
-        return
-
-    return rows
-
-def load_csv_to_table(conn, csv_path, table_name):
-    rows = read_csv_rows(csv_path=csv_path)
-
-    columns = rows[0].keys()
-    col_list = ", ".join(columns)
-    placeholders = ", ".join(["?"] * len(columns))
-    sql = f"INSERT INTO {table_name} ({col_list}) VALUES ({placeholders})"
-
-    values = [tuple(row[col] for col in columns) for row in rows]
-    conn.executemany(sql, values)
-    conn.commit()
+    class Meta:
+        table_name = TABLE_ITEMS
 
 if __name__ == "__main__":
-    conn = recreate_db()
+
+    logger.info("Starting database build")
+
+    if SQLITE_DATABASE.exists():
+        logger.info("Old database file removed")
+        SQLITE_DATABASE.unlink()
+
+    db.connect()
+    db.create_tables([Item])
 
     for csv_path in IMPORT_DATA_DIRECTORY.glob("*.csv"):
         table_name = csv_path.stem
-        print(f"Loading {csv_path} into {table_name}")
+        logger.info(f"Importing data to '{table_name}' table")
 
-        match table_name:
-            case "items":
-                print("Do items import")
-            case _:
-                print("Unsupported file, failure")
-                sys.exit(1)
+        rows = []
+        with open(csv_path, newline="", encoding="utf-8") as f:
+            logger.info(f"File opened {csv_path}")
+            reader = csv.DictReader(f)
+            for r in reader:
+                row = MAPPERS[table_name](r)
+                rows.append(row)
+            logger.info(f"Found {len(rows)} rows for import")
 
-        load_csv_to_table(conn, csv_path, table_name)
-
-    conn.close()
+        result = Item.insert_many(rows).execute()
+        if result != len(rows):
+            logger.warning("Not all items were inserted")
+        else:
+            logger.info(f"All items successfully imported to {table_name}")
---
+++ src/db/utils/mappers.py
@@ -0,0 +1,22 @@
+import re
+
+from src.db.consts import TABLE_ITEMS
+
+def slugify(text: str) -> str:
+    text = text.lower().strip()
+    text = re.sub(r'[^a-z0-9]+', '-', text)
+    return text
+
+def map_item(item):
+
+    slug = slugify(item["name"])
+
+    return {
+        "id": item["id"],
+        "name": item["name"],
+        "slug": slug,
+    }
+
+MAPPERS = {
+    TABLE_ITEMS: map_item
+}
